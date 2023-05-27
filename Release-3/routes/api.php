bf625219bde87a7a3cb9356c43d0991223b8a32b
KennedyDT <126506608+KennedyDT@users.noreply.github.com>
2023-05-26 23:02:28 -0500

    correcciones

--- Release-3/routes/api.php
+++ Release-3/routes/api.php
@@ -15,16 +15,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   //   Route::get('productos', 'index');
     // Route::post('productos', 'store');
     // Route::get('productos/{id}', 'show');
-     
+
      //Route::put('productos/{id}', 'update');
      //Route::delete('productos/{id}', 'destroy');
 
 //});
 
 
-Route::delete('/Productos/{id}', [ProductosController::class, 'destroy'])->name('Productos.update');
+Route::delete('/Productos/{id}', [ProductosController::class, 'destroy'])->name('Productos.destroy');
 Route::put('/Productos/{id}', [ProductosController::class, 'update'])->name('Productos.update');
-Route::get('/Productos/{id}', [ProductosControllerr::class, 'show'])->name('Productos.create');
+Route::get('/Productos/{id}', [ProductosController::class, 'show'])->name('Productos.create');
 Route::post('/Productos', [ProductosController::class, 'store'])->name('Productos.store');
 Route::get('/Productos', [ProductosController::class, 'index'])->name('Productos.index');
 
